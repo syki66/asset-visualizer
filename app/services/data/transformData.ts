fe300bddc9552b73c2c4c16db086173e5ffb2715
syki66 <66syki@gmail.com>
2025-01-03 21:02:11 +0900

    feat: timestamp를 YYYY-MM-DD로 변경하는 함수 추가

--- app/services/data/transformData.ts
+++ app/services/data/transformData.ts
@@ -1,10 +1,12 @@
+import { formatDate } from '@/utils/format';
+
 // 야후 금융 API로부터 데이터 변환 (key: timestamp, value: 조정종가)
 export function transformYahooHistoryData(data: any) {
   const { timestamp, indicators } = data.chart.result[0];
   const adjClose = indicators.adjclose[0].adjclose;
 
   return timestamp.map((timestamp: number, index: number) => ({
-    timestamp: timestamp,
+    timestamp: formatDate(timestamp),
     adjClose: adjClose[index],
   }));
 }
---
+++ utils/format.ts
@@ -0,0 +1,8 @@
+// timestamp를 YYYY-MM-DD 형식으로 변환
+export const formatDate = (timestamp: number) => {
+  const date = new Date(timestamp * 1000);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
